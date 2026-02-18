c300784df51958f7bfcc6dc0715944fb3ff9d9fd
RidvanPeshkopia <ridvanpeshkopia1@gmail.com>
2026-02-18 14:37:40 +0100

    Fix: Backend listens on PORT env var

--- backend/server.py
+++ backend/server.py
@@ -15,11 +15,12 @@ class TempConvService(tempconv_pb2_grpc.TempConvServiceServicer):
         return tempconv_pb2.TempResponse(value=c)
 
 def serve():
+    port = os.environ.get('PORT', '50051')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     tempconv_pb2_grpc.add_TempConvServiceServicer_to_server(TempConvService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
-    print("Server started on port 50051")
+    print(f"Server started on port {port}")
     server.wait_for_termination()
 
 if __name__ == '__main__':
